972872f348625be7ecd88e5b3b947c8444b0654c
Huberto Kaiser Filho <hubertokf@gmail.com>
2018-06-15 04:14:44 -0300

    another otimization on run script

--- run.sh
+++ run.sh
@@ -1,52 +1,20 @@
 header="algoritmo,heuristica,tamanho,embaralhamento,profundidade,tempo,memoria"
+echo $header >> result.csv
 
-for qt in 10 20 30 40 50
+for search in "a* misplaced" "a* manhattan" "ids" "dfs" "bfs"
 do
-    for s in 2 3
+    for moves in 10 20 30 40 50
     do
-        i=0
-        echo "running $s x $s bfs $qt"
-        while [ $i -lt 10 ]
+        for size in 2 3
         do
-            out=$(python3 main.py $s $qt bfs 2>&1)
-            echo $out >> result.csv
-            i=$[$i+1]
-        done
-
-        echo "running $s x $s dfs $qt"
-        i=0
-        while [ $i -lt 10 ]
-        do
-            out=$(python3 main.py $s $qt dfs 2>&1)
-            echo $out >> result.csv
-            i=$[$i+1]
-        done
-
-        echo "running $s x $s ids $qt"
-        i=0
-        while [ $i -lt 10 ]
-        do
-            out=$(python3 main.py $s $qt ids 2>&1)
-            echo $out >> result.csv
-            i=$[$i+1]
-        done
-
-        echo "running $s x $s a*(manhattan) $qt"
-        i=0
-        while [ $i -lt 10 ]
-        do
-            out=$(python3 main.py $s $qt a* manhattan 2>&1)
-            echo $out >> result.csv
-            i=$[$i+1]
-        done
-
-        echo "running $s x $s a*(misplaced) $qt"
-        i=0
-        while [ $i -lt 10 ]
-        do
-            out=$(python3 main.py $s $qt a* misplaced 2>&1)
-            echo $out >> result.csv
-            i=$[$i+1]
+            echo "running $size x $size $search $moves"
+            i=0
+            while [ $i -lt 10 ]
+            do
+                out=$(python3 main.py $size $moves $search 2>&1)
+                echo $out >> result.csv
+                i=$[$i+1]
+            done
         done
     done
 done
